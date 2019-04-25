8efbb0a4624eaf5e5df30f854b92761f07eab4b9
Alexander Schelchere <ascellie@gmail.com>
2019-04-25 01:34:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(obj, key, val) {
+  Object.
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, val) {
+  
+}
+
+function deleteFromObjectByKey(obj, key {
+  
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key) {
   
 }
