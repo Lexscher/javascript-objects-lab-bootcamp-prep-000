8b31aefacdea9b5bda22c4e01fe413d95fdc948f
Alexander Schelchere <ascellie@gmail.com>
2019-04-25 01:47:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,16 +5,11 @@ function updateObjectWithKeyAndValue(obj, key, val) {
   return newObject;
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(obj, key, val) {
-  obj.key = val;
-  return obj;
-}
-
-function updateObjectWithKeyAndValue(obj, key, val) {
-  var newObject = Object.assign({}, obj);
-  newObject[key] = val;
-  return newObject;
-}
+// function updateObjectWithKeyAndValue(obj, key, val) {
+//   var newObject = Object.assign({}, obj);
+//   newObject[key] = val;
+//   return newObject;
+// }
 
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, val) {
   obj.key = val;
